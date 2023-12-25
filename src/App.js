961297e55e350a798d6dc3c050a40c1cb036a992
Aryan Dwivedi <aryan.735921@gmail.comm>
2023-12-25 13:28:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navigation from './Customer/Component/Navigation/Navigation';
+import Homepage from './Customer/pages/Homepage/Homepage';
+import Footer from './Customer/Component/Footer/Footer';
+import Product from './Customer/Component/Products/Product';
+import ProductDetails from './Customer/Component/productDetails/ProductDetails';
+import Cart from './Customer/Component/Cart/Cart';
+import CheckOut from './Customer/Component/Checkout/CheckOut';
+import Order from './Customer/Component/Order/Order';
+import OrderDetails from './Customer/Component/Order/OrderDetails';
+import { Route, Routes } from 'react-router-dom';
+import CustomerRoutes from './Routers/CustomerRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        <Route path='/*' element={<CustomerRoutes/>}></Route>
+      </Routes>
+      <div>
+        
+        <div>
+          {/* <Homepage/> */}
+        
+         
+        </div>
+      </div>
+      
     </div>
   );
 }
